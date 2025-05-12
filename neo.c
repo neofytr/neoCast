3ffb74e1de5ebb0e0c92a4be5e19b9ac838cfa52
neofytr <imagination12344321.com@gmail.com>
2025-05-12 06:29:20 +0530

    some changes

--- neo.c
+++ neo.c
@@ -4,5 +4,8 @@
 int main(int argc, char **argv)
 {
     neorebuild("neo.c", argv);
+    neo_compile_to_object_file(GCC, "main.c", "main.o", NULL, false);
+    neo_link(GCC, "main", NULL, false, "main.o");
+    remove("main.o");
     return EXIT_SUCCESS;
 }
