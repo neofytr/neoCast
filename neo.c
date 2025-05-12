db35104007e55a4f6605f09d7fbcf386fbe05c4b
neofytr <imagination12344321.com@gmail.com>
2025-05-12 18:55:47 +0530

    changes

--- neo.c
+++ neo.c
@@ -4,11 +4,19 @@
 int main(int argc, char **argv)
 {
     neorebuild("neo.c", argv);
-    neo_compile_to_object_file(GCC, "./client/client.c", NULL, NULL, true);
-    neo_compile_to_object_file(GCC, "./server/server.c", NULL, NULL, true);
-    neo_link(GCC, "client_test", NULL, true, "./client/client.o");
-    neo_link(GCC, "server_test", NULL, true, "./server/server.o");
+    if (argc > 1 && !strcmp(argv[1], "clean"))
+    {
+        remove("client_test");
+        remove("server_test");
+        return EXIT_SUCCESS;
+    }
+
+    neo_compile_to_object_file(GCC, "client/client.c", NULL, NULL, true);
+    neo_compile_to_object_file(GCC, "server/server.c", NULL, NULL, true);
+    neo_link(GCC, "client_test", NULL, true, "client/client.o");
+    neo_link(GCC, "server_test", NULL, true, "server/server.o");
     remove("client/client.o");
     remove("server/server.o");
+
     return EXIT_SUCCESS;
 }
